eae97f66a8a1ab5a8a078608b3d0a209470833ea
Madsbert <mberthelsen@live.dk>
2025-05-19 21:42:45 +0200

    small stuff

--- src/main/java/org/example/jobswap/View/JobswapApplication.java
+++ src/main/java/org/example/jobswap/View/JobswapApplication.java
@@ -6,12 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class JobswapApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JobswapApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load((getClass().getResource("/org/example/jobswap/View/Login.fxml"))));
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

d8a3edec4e4d5d485337d4e9085a717010219858
Madsbert <mberthelsen@live.dk>
2025-05-19 21:16:29 +0200

    added the database connection method

--- src/main/java/org/example/jobswap/Foundation/DBConnection.java
+++ src/main/java/org/example/jobswap/Foundation/DBConnection.java
@@ -1,9 +1,34 @@
 package org.example.jobswap.Foundation;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnection {
-    public static Connection getConnection() {
-        return null;
-    }
+
+        private static final String URL = "jdbc:sqlserver://localhost;databaseName=KantineDB";
+        private static final String USERNAME = "sa"; // replace with your username
+        private static final String PASSWORD = "Kantine1234"; // replace with your password
+        private static Connection conn;
+
+        /**
+         * Establishes connection to the Database.
+         * @return return the connection to the Database.
+         */
+        public static Connection getConnection() {
+            if (conn == null) {
+                try
+                {
+                    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                    conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                    System.out.println("Connected to the database.");
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Failed to connect to database.");
+                }
+            }
+            return conn;
+        }
+
 }
+

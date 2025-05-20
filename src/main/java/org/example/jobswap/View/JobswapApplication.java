7cfa2577f71e8b63a270d0fd52f7c60e9526361b
Madsbert <mberthelsen@live.dk>
2025-05-20 09:24:11 +0200

    added jar file and made databse work

--- src/main/java/org/example/jobswap/View/JobswapApplication.java
+++ src/main/java/org/example/jobswap/View/JobswapApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.jobswap.Foundation.DBConnection;
 
 import java.io.IOException;
 import java.util.Objects;

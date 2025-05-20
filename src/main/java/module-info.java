ba1fb7678be7423138366812e363468414e20857
Madsbert <mberthelsen@live.dk>
2025-05-20 07:53:50 +0200

    Made the thing run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,6 @@ module org.example.jobswap {
 
 
     opens org.example.jobswap to javafx.fxml;
-    exports org.example.jobswap;
     exports org.example.jobswap.Controllers;
     opens org.example.jobswap.Controllers to javafx.fxml;
     exports org.example.jobswap.View;
--- src/main/java/org/example/jobswap/View/JobswapApplication.java
+++ src/main/java/org/example/jobswap/View/JobswapApplication.java
@@ -11,8 +11,8 @@ import java.util.Objects;
 public class JobswapApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JobswapApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load((getClass().getResource("/org/example/jobswap/View/Login.fxml"))));
+        FXMLLoader fxmlLoader = new FXMLLoader(JobswapApplication.class.getResource("/org/example/jobswap/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

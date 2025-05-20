dad0f8fe6ffe60717e2f1ab5b3cfb097f7102cff
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2025-05-20 16:06:52 +0200

    Finished non functional UI for UserTabProfile

--- src/main/java/org/example/jobswap/Controllers/UserTabs/UserTabProfile.java
+++ src/main/java/org/example/jobswap/Controllers/UserTabs/UserTabProfile.java
@@ -1,29 +1,24 @@
 package org.example.jobswap.Controllers.UserTabs;
 
-import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
 import org.example.jobswap.Controllers.MainSceneController;
-import org.example.jobswap.Model.Department;
-import org.example.jobswap.Model.Profile;
 import org.example.jobswap.Service.BorderedVBox;
 import org.example.jobswap.Service.Header;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class UserTabProfile extends javafx.scene.control.Tab {
 
     private boolean editMode = false;
-    private ChoiceBox departmentLabel;
-    private TextField jobCategoryLabel;
+    private ChoiceBox departmentChoiceBox;
+    private ChoiceBox jobCategoryChoiceBox;
+    private TextField jobTitleField;
 
     public UserTabProfile() {
         super("Profile");
@@ -36,43 +31,80 @@ public class UserTabProfile extends javafx.scene.control.Tab {
         VBox vbox = new VBox();
         scrollPane.setContent(vbox);
 
-        VBox matchVBox = new BorderedVBox();
-        matchVBox.getChildren().add(new Header("Personal details"));
-        vbox.getChildren().add(matchVBox);
+        VBox profileDetailsBox = new BorderedVBox();
+        profileDetailsBox.getChildren().add(new Header("Personal details"));
+        vbox.getChildren().add(profileDetailsBox);
 
         HBox nameBox = new HBox();
 
         Label tLabel = new Label("Name:");
         tLabel.setPrefWidth(140);
-        tLabel.setStyle("-fx-padding: 0 20 0 10;");
+        tLabel.setStyle("-fx-padding: 0 0 0 10;");
 
         nameBox.getChildren().add(tLabel);
         nameBox.getChildren().add(new Label(MainSceneController.getCurrentProfile().getName()));
-        matchVBox.getChildren().add(nameBox);
+        profileDetailsBox.getChildren().add(nameBox);
 
         HBox idBox = new HBox();
 
         Label t2Label = new Label("ID:");
         t2Label.setPrefWidth(140);
-        t2Label.setStyle("-fx-padding: 0 20 0 10;");
+        t2Label.setStyle("-fx-padding: 0 0 0 10;");
 
         idBox.getChildren().add(t2Label);
         idBox.getChildren().add(new Label("" + MainSceneController.getCurrentProfile().getProfileID()));
-        matchVBox.getChildren().add(idBox);
+        profileDetailsBox.getChildren().add(idBox);
 
         HBox departmentBox = new HBox();
         ArrayList<String> departments = new ArrayList<>();
         departments.add("Sønderborg");
-        departmentLabel = new ChoiceBox();
-        departmentLabel.getItems().addAll(departments);
+        departmentChoiceBox = new ChoiceBox();
+        departmentChoiceBox.getItems().addAll(departments);
 
         Label t3Label = new Label("Departments:");
         t3Label.setPrefWidth(140);
-        t3Label.setStyle("-fx-padding: 0 20 0 10;");
+        t3Label.setStyle("-fx-padding: 0 0 0 10;");
 
         departmentBox.getChildren().add(t3Label);
-        departmentBox.getChildren().add(departmentLabel);
-        matchVBox.getChildren().add(departmentBox);
+        departmentBox.getChildren().add(departmentChoiceBox);
+        profileDetailsBox.getChildren().add(departmentBox);
+
+
+        HBox jobCategoryBox = new HBox();
+        ArrayList<String> jobCategories = new ArrayList<>();
+        jobCategories.add("Sanitet");
+        jobCategories.add("Produktion");
+        jobCategories.add("Pakkeri");
+        jobCategoryChoiceBox = new ChoiceBox();
+        jobCategoryChoiceBox.getItems().addAll(jobCategories);
+        for (int i = 0; i < jobCategories.size(); i++) {
+            if (MainSceneController.getCurrentProfile().getJobCategory().equals(jobCategories.get(i))) {
+                jobCategoryChoiceBox.getSelectionModel().select(i);
+            }
+        }
+
+        Label t4Label = new Label("Job Category:");
+        t4Label.setPrefWidth(140);
+        t4Label.setStyle("-fx-padding: 0 0 0 10;");
+
+        jobCategoryBox.getChildren().add(t4Label);
+        jobCategoryBox.getChildren().add(jobCategoryChoiceBox);
+        profileDetailsBox.getChildren().add(jobCategoryBox);
+
+
+
+        HBox JobTitleBox = new HBox();
+
+        jobTitleField = new TextField(MainSceneController.getCurrentProfile().getJobTitle());
+
+        Label t5Label = new Label("Job Title:");
+        t5Label.setPrefWidth(140);
+        t5Label.setStyle("-fx-padding: 0 0 0 10;");
+
+        JobTitleBox.getChildren().add(t5Label);
+        JobTitleBox.getChildren().add(jobTitleField);
+        profileDetailsBox.getChildren().add(JobTitleBox);
+
 
         this.setContent(scrollPane);
     }

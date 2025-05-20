0060739a58003e05e36fd3cfb3466ce46dd529bc
Madsbert <mberthelsen@live.dk>
2025-05-20 10:05:29 +0200

    Made shift scene

--- src/main/java/org/example/jobswap/Controllers/LoginController.java
+++ src/main/java/org/example/jobswap/Controllers/LoginController.java
@@ -1,13 +1,25 @@
 package org.example.jobswap.Controllers;
 
+import javafx.event.ActionEvent;
+import org.example.jobswap.Controllers.UserTabs.UserTabMatches;
+import org.example.jobswap.Service.SceneService;
+
+import java.io.IOException;
+
 public class LoginController {
+
     private void checkCredentials(String workerId, String password)
     {
 
     }
 
-    private void shiftScene()
-    {
+    public void login(ActionEvent event) throws IOException {
 
+        UserTabMatches userTabMatches = new UserTabMatches();
+        shiftScene(event, "Jobswap", userTabMatches.getClass());
+    }
+
+    private void shiftScene(ActionEvent event, String title, Class controller) throws IOException {
+        SceneService.shiftScene(event, title, controller);
     }
 }
--- src/main/java/org/example/jobswap/Service/SceneService.java
+++ src/main/java/org/example/jobswap/Service/SceneService.java
@@ -1,9 +1,27 @@
 package org.example.jobswap.Service;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.example.jobswap.View.JobswapApplication;
+
+import java.io.IOException;
+
 
 public class SceneService {
-    public void shiftScene(Scene scene){
+    public static void shiftScene(ActionEvent actionEvent, String title, Class controller ) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(JobswapApplication.class.getResource("/org/example/jobswap/MainScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
+        // Get controller
+         controller = fxmlLoader.getController();
+         //need a method for initizializing the scene
+
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
     }
 }

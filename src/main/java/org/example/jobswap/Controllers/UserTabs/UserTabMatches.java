a9f295a18f84de1745e650a550b34b43e5082a5e
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2025-05-22 14:20:55 +0200

    Updated Jobswap
    JobSwap tab now allows to select department and see other users

--- src/main/java/org/example/jobswap/Controllers/UserTabs/UserTabMatches.java
+++ src/main/java/org/example/jobswap/Controllers/UserTabs/UserTabMatches.java
@@ -72,24 +72,7 @@ public class UserTabMatches extends javafx.scene.control.Tab {
      */
     private void showMatchApplications()
     {
-        List<Profile> matchingProfiles = MatchDB.seekAllPossibleProfileMatches(MainSceneController.getCurrentProfile().getProfileID(),
-                MainSceneController.getCurrentProfile().getDepartment());
 
-        List<HBox> matchingProfilesHBoxes = new ArrayList<>();
-
-        for (Profile profile : matchingProfiles) {
-            HBox hbox = new HBox();
-            hbox.setSpacing(10);
-            hbox.setPadding(new Insets(25, 25, 25, 25));
-            hbox.getChildren().add(new Label("Username: " + profile.getUsername()));
-            hbox.getChildren().add(new Label("Department: " + profile.getDepartment()));
-            hbox.getChildren().add(new Label("Job Titel: " + profile.getJobTitle()));
-            hbox.getChildren().add(new Label("Job Description: " + profile.getJobDescription()));
-
-            matchingProfilesHBoxes.add(hbox);
-        }
-
-        applicationsVBox.getChildren().addAll(matchingProfilesHBoxes);
     }
 
     private void showBothInterested()
--- src/main/java/org/example/jobswap/Controllers/UserTabs/UserTabSeekJobSwap.java
+++ src/main/java/org/example/jobswap/Controllers/UserTabs/UserTabSeekJobSwap.java
@@ -9,11 +9,14 @@ import javafx.scene.layout.VBox;
 import org.example.jobswap.Controllers.MainSceneController;
 import org.example.jobswap.Model.AccessLevel;
 import org.example.jobswap.Model.Department;
+import org.example.jobswap.Model.Profile;
 import org.example.jobswap.Persistence.DepartmentDB;
+import org.example.jobswap.Persistence.MatchDB;
 import org.example.jobswap.Service.BorderedVBox;
 import org.example.jobswap.Service.Header;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * class which sets up the tab seek jobswap
@@ -22,6 +25,9 @@ public class UserTabSeekJobSwap extends javafx.scene.control.Tab {
 
     private static VBox departmentBox;
     private static ChoiceBox<String> departmentChoiceBox;
+
+    private static VBox availableSwapsBox;
+    private static VBox swapsBox;
 
     public UserTabSeekJobSwap()
     {
@@ -37,11 +43,18 @@ public class UserTabSeekJobSwap extends javafx.scene.control.Tab {
 
         departmentBox = new BorderedVBox();
         departmentBox.getChildren().add(new Header("Choose Department"));
+        departmentBox.setPrefHeight(120);
         vbox.getChildren().add(departmentBox);
 
-        VBox applicationsVBox = new BorderedVBox();
-        applicationsVBox.getChildren().add(new Header("Potential Matches"));
-        vbox.getChildren().add(applicationsVBox);
+        availableSwapsBox = new BorderedVBox();
+        availableSwapsBox.getChildren().add(new Header("Potential Matches"));
+
+        swapsBox = new VBox();
+        availableSwapsBox.getChildren().add(swapsBox);
+
+        vbox.getChildren().add(availableSwapsBox);
+
+
 
         this.setContent(scrollPane);
 
@@ -50,32 +63,57 @@ public class UserTabSeekJobSwap extends javafx.scene.control.Tab {
 
     private void showJobswapOptions()
     {
-        ArrayList<String> departments = new ArrayList<>();
-
-        for (Department department : DepartmentDB.getDepartments())
-        {
-            departments.add(department.getDepartmentName());
-        }
-
-        departmentChoiceBox = new ChoiceBox<String>();
-        departmentChoiceBox.getItems().addAll(departments);
-
         Label label = new Label("Departments:");
         label.setPrefWidth(140);
         label.setStyle("-fx-padding: 0 0 0 10;");
-
         departmentBox.getChildren().add(label);
+
+
+        ArrayList<String> departments = new ArrayList<>();
+        for (Department department : DepartmentDB.getDepartments())
+        {
+            if (MainSceneController.getCurrentProfile().getDepartment().equals(department.getDepartmentName()))
+            {
+                continue;
+            }
+            departments.add(department.getDepartmentName());
+        }
+
+        departmentChoiceBox = new ChoiceBox<>();
+        departmentChoiceBox.getItems().addAll(departments);
+        departmentChoiceBox.setOnAction(event -> {updateJobList();});
         departmentBox.getChildren().add(departmentChoiceBox);
+
+        updateJobList();
     }
 
-    private void setSelectedDepartment(int department)
+    private void updateJobList()
     {
+        if (departmentChoiceBox.getSelectionModel().isEmpty() && departmentChoiceBox.getSelectionModel().getSelectedItem() == null)
+        {
+            return;
+        }
 
-    }
+        swapsBox.getChildren().clear();
 
-    private void setSelectedJobcategory(int jobCategory)
-    {
+        List<Profile> matchingProfiles = MatchDB.seekAllPossibleProfileMatches(MainSceneController.getCurrentProfile().getProfileID(),
+                departmentChoiceBox.getSelectionModel().getSelectedItem());
 
+        List<HBox> matchingProfilesHBoxes = new ArrayList<>();
+
+        for (Profile profile : matchingProfiles) {
+            HBox hbox = new HBox();
+            hbox.setSpacing(10);
+            hbox.setPadding(new Insets(25, 25, 25, 25));
+            hbox.getChildren().add(new Label("Username: " + profile.getUsername()));
+            hbox.getChildren().add(new Label("Department: " + profile.getDepartment()));
+            hbox.getChildren().add(new Label("Job Titel: " + profile.getJobTitle()));
+            hbox.getChildren().add(new Label("Job Description: " + profile.getJobDescription()));
+
+            matchingProfilesHBoxes.add(hbox);
+        }
+
+        swapsBox.getChildren().addAll(matchingProfilesHBoxes);
     }
 
     private void applyForJobswap()

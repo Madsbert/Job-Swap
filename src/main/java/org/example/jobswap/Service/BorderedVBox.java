4defd328cface15b3df90a43ac2dab53f69d9d4f
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2025-05-22 13:10:37 +0200

    Updated comments

--- src/main/java/org/example/jobswap/Service/BorderedVBox.java
+++ src/main/java/org/example/jobswap/Service/BorderedVBox.java
@@ -2,6 +2,9 @@ package org.example.jobswap.Service;
 
 import javafx.scene.layout.VBox;
 
+/**
+ * {@link VBox} with a sliver border Standard for encapturing titled areas.
+ */
 public class BorderedVBox extends VBox {
     public BorderedVBox() {
         super();
